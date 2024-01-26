eb91d18f01cf7991ecaea7f4fcc9f4a9a0f0c486
Patrick Wilkie <plwilkie@gmail.com>
2024-01-26 03:42:01 -0500

    Fix typo in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     assetsInclude: ['**/*.glb'],
-    base: "/cryptic-mystic-dragon-automaton"
+    base: "/cryptic-mystic-dragon-automaton/"
 })
